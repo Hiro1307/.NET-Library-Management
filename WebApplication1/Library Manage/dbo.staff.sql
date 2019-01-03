07574ce8ef2a4d20705018d3c451d00d944db10b
Muhammad Nor Iman bin Abdul Manan <imanmanan@gmail.com>
2019-01-04 01:16:17 +0800

    try try again

---
+++ WebApplication1/Library Manage/dbo.staff.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[staff] (
+    [staffID]  INT           NOT NULL,
+    [userName] VARCHAR (50)  NOT NULL,
+    [name]     VARCHAR (50)  NOT NULL,
+    [password] NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([staffID] ASC)
+);
+
