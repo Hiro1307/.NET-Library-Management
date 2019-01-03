ae98947fe98730e281ce275a0ade84558f914f5c
sarafudin said <sarafudin97@gmail.com>
2019-01-04 02:22:51 +0800

    hmm

---
+++ WebApplication1/Limas2.0/dbo.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[staff]
+(
+	[staffId] INT NOT NULL PRIMARY KEY, 
+    [userName] VARCHAR(50) NOT NULL, 
+    [name] VARCHAR(50) NOT NULL, 
+    [password] VARCHAR(50) NOT NULL
+)
